a834c16775a8a705d248816990acf020cf930adf
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-01-01 18:13:53 -0500

    adding clarity notes

--- routers/index.js
+++ routers/index.js
@@ -24,3 +24,5 @@ router
     .use("/member/todo", require("./todo"))
 
 module.exports = router;
+
+// the index router acts as the central routing system, here there is a router created by express and we are going to require the other routers that are also in the same directory so that we can use them.
